2461304c4c7f639866726595ab43133533a3f5f1
Anders Porsbo <apor@tv2.dk>
2023-11-27 04:27:35 +0100

    removed s3 block

--- terraform/s3-public-block.tf
+++ terraform/s3-public-block.tf
@@ -1,4 +1,18 @@
-resource "aws_s3_account_public_access_block" "global-block" {
-	block_public_acls = true
-	block_public_policy = true
-}
+#resource "aws_s3_account_public_access_block" "global-block" {
+#	block_public_acls = true
+#	block_public_policy = true
+#}
+
+# test by adding this to bucket policy
+#{
+#    "Version": "2012-10-17",
+#    "Statement": [
+#        {
+#            "Sid": "public-read",
+#            "Effect": "Allow",
+#            "Principal": "*",
+#            "Action": "s3:GetObject",
+#            "Resource": "arn:aws:s3:::tv2-883245328735-test-bucket-create-public-before-scp/*"
+#        }
+#    ]
+#}
