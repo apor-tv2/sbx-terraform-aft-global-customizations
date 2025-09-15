b2ed32a45dafc5f4645ac38003526a0ee92f65e8
Anders Porsbo <apor@tv2.dk>
2025-09-15 12:54:09 +0200

    wip

--- terraform/ssm_parameters.tf
+++ terraform/ssm_parameters.tf
@@ -1,9 +1,29 @@
+
+output "which_accountid_aft_management"{
+	value = data.aws_caller_identity.which_accountid_aft_management
+}
+data "aws_caller_identity" "which_accountid_aft_management"{
+  provider = aws.aft-management
+}
+output "which_accountid_default"{
+	value = data.aws_caller_identity.which_accountid_default
+}
+data "aws_caller_identity" "which_accountid_default"{
+}
+output "which_accountid_eucentral1"{
+	value = data.aws_caller_identity.which_accountid_eucentral1
+}
+data "aws_caller_identity" "which_accountid_eucentral1"{
+  provider = aws.eu-central-1
+}
+
 data "aws_ssm_parameter" "aft_account_id" {
   provider = aws.aft-management
   name = "/aft/account/aft-management/account-id"
 }
 
 resource "aws_ssm_parameter" "aft_account_id"{
+  provider = aws.eu-central-1
   name = "/sbxtest/aft/account/aft-management/account-id"
   type = "String"
   value = data.aws_ssm_parameter.aft_account_id.value
