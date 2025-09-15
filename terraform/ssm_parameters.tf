2c0f134edc3239354f1a8baa16428a7731ef9ce6
Anders Porsbo <apor@tv2.dk>
2025-09-15 10:45:01 +0200

    trying to copy ssm parameters from aft account into another account

---
+++ terraform/ssm_parameters.tf
@@ -0,0 +1,11 @@
+data "aws_ssm_parameter" "aft_account_id" {
+  provider = aws.aft-management
+  name = "/aft/account/aft-management/account-id"
+  type = "String"
+}
+
+resource "aws_ssm_parameter" "aft_account_id"{
+  name = "/sbxtest/aft/account/aft-management/account-id"
+  type = "String"
+  value = data.aws_ssm_parameter.aft_account_id
+}
